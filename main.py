bb4a48d2e2307645e633751f43852eec1a455490
N1nj4R8 <s1lntlwd@proton.me>
2023-05-10 19:30:55 +0330

    Add icon for app and

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-import PySide2.QtGui as QtGui
-import PySide2.QtWidgets as QtWidgets
+from PySide2.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PySide2.QtCore import Qt, QSize
+from PySide2.QtGui import QIcon
 import sys
 
-window = QtWidgets.QWidget()
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Main
+        self.setWindowTitle("Image Viewer")
+        self.setMinimumSize(QSize(360, 360))
+        self.setWindowIcon(QIcon("icon.png"))
+
+        # Layout
+        layout = QVBoxLayout()
+
+        # Container
+        widgets = QWidget()
+        widgets.setLayout(layout)
+
+        self.setCentralWidget(widgets)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec_()
